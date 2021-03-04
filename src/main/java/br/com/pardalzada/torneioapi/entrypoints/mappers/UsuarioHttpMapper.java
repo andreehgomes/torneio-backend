5c987c0ab6695753efb1f09fe08a348b283fbfc5
andreehgomes <andrefelipefeliciogomes@gmail.com>
2021-03-03 23:06:10 -0300

    Ajusta para retornar a senha do usuário

--- src/main/java/br/com/pardalzada/torneioapi/entrypoints/mappers/UsuarioHttpMapper.java
+++ src/main/java/br/com/pardalzada/torneioapi/entrypoints/mappers/UsuarioHttpMapper.java
@@ -17,6 +17,7 @@ public class UsuarioHttpMapper extends FromToMapper<UsuarioHttp, UsuarioEntity> 
         return UsuarioHttp.builder()
                 .codigo(usuarioEntity.getCodigo())
                 .email(usuarioEntity.getEmail())
+                .senha(usuarioEntity.getSenha())
                 .tipo(usuarioEntity.getTipo())
                 .permissoes(permissaoHttpMapper.fromList(usuarioEntity.getPermissoes()))
                 .build();
